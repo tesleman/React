17d5b3a6fc41fa55d686abdcb5c76125785e7f04
tesleman <54055976+tesleman@users.noreply.github.com>
2020-06-11 11:24:12 +0300

    commit3

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ let Footer = () =>{
                         <div className="col l4 offset-l2 s12">
                             <h5 className="white-text">Links</h5>
                             <ul>
-                                <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                                <li><a className="grey-text text-lighten-3" href="#">Link 1</a></li>
 
                             </ul>
                         </div>
@@ -24,7 +24,7 @@ let Footer = () =>{
                 <div className="footer-copyright">
                     <div className="container">
                         © 2020 Copyright Text
-                        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                        <a className="grey-text text-lighten-4 right" href="#">More Links</a>
                     </div>
                 </div>
             </footer>
