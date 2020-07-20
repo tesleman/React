9bcf2a1d5597ce29d80142a40f56e9c82f959bde
tesleman <54055976+tesleman@users.noreply.github.com>
2020-07-20 13:42:38 +0300

    Updates

---
+++ precache-manifest.1da349dd7481b5d97323f3214e516893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbdf8fccdc9cf39d2e28b6c04a4c603",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "918ae5ddefd9afa9acf3",
+    "url": "/React/static/css/2.0732034e.chunk.css"
+  },
+  {
+    "revision": "3138eb595694b2e3791c",
+    "url": "/React/static/css/main.556962be.chunk.css"
+  },
+  {
+    "revision": "918ae5ddefd9afa9acf3",
+    "url": "/React/static/js/2.bdb32863.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/static/js/2.bdb32863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3138eb595694b2e3791c",
+    "url": "/React/static/js/main.8df73a84.chunk.js"
+  },
+  {
+    "revision": "a6e13c67e41b68ea7d7b",
+    "url": "/React/static/js/runtime-main.79693f35.js"
+  },
+  {
+    "revision": "b4740094504210aae458ab21dfec2f59",
+    "url": "/React/static/media/simon-zhu-iaJH3_7EXSg-unsplash.b4740094.jpg"
+  }
+]);
